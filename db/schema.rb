2092a944a48ced68f660e74e7d4e392adac94d40
fabianrios <hola@fabianrios.co>
2016-01-04 21:03:56 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216075002) do
+ActiveRecord::Schema.define(version: 20160105020148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer  "identification"
+    t.string   "period"
+    t.integer  "went_days"
+    t.integer  "skip_days"
+    t.text     "reason"
+    t.integer  "student_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "attendances", ["student_id"], name: "index_attendances_on_student_id", using: :btree
+
+  create_table "establishments", force: :cascade do |t|
+    t.integer  "code"
+    t.string   "name"
+    t.string   "department"
+    t.string   "state"
+    t.string   "phone"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "guardians", force: :cascade do |t|
     t.string   "id_type"
@@ -36,6 +60,52 @@ ActiveRecord::Schema.define(version: 20151216075002) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
+
+  create_table "health_cares", force: :cascade do |t|
+    t.boolean  "affiliate"
+    t.string   "regime"
+    t.string   "eps"
+    t.string   "ips"
+    t.boolean  "register"
+    t.string   "card"
+    t.date     "poll_date"
+    t.float    "score"
+    t.boolean  "inhabilites"
+    t.text     "inh_description"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.integer  "code"
+    t.string   "name"
+    t.string   "address"
+    t.string   "neighborhood"
+    t.string   "zone"
+    t.string   "contact_name"
+    t.string   "contact_position"
+    t.string   "phone"
+    t.string   "email"
+    t.boolean  "headquarter"
+    t.boolean  "foundation_present"
+    t.integer  "establishment_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "schools", ["establishment_id"], name: "index_schools_on_establishment_id", using: :btree
+
+  create_table "scores", force: :cascade do |t|
+    t.integer  "identification"
+    t.string   "period"
+    t.string   "area"
+    t.string   "score"
+    t.integer  "student_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "scores", ["student_id"], name: "index_scores_on_student_id", using: :btree
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
@@ -79,5 +149,8 @@ ActiveRecord::Schema.define(version: 20151216075002) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "attendances", "students"
+  add_foreign_key "schools", "establishments"
+  add_foreign_key "scores", "students"
   add_foreign_key "students", "guardians"
 end
