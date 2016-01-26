8204332fd78519dad7cb8c5ddf4142a6d251ef7c
fabianrios <hola@fabianrios.co>
2016-01-25 22:46:24 -0500

    revisiones frontales

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,14 +1,18 @@
 class VisitorsController < ApplicationController
   def index
+    @schools = School.all
     if params[:search]
       s = "%#{params[:search]}%"
-      if params[:search].to_i.is_a?(Integer)  && params[:search].to_i > 0
+      i = "%#{params[:instituciones]}%"
+      if params[:search].to_i.is_a?(Integer)  && params[:search].to_i > 0 && !i.present?
         @students = Student.where(identification: params[:search].to_i)
+      elsif params[:search].to_i.is_a?(Integer)  && params[:search].to_i > 0 && i.present?
+        @students = Student.joins(:schools).where(identification: params[:search].to_i)
       else
         @students = Student.where("lower(name) LIKE :search", search: s.downcase)
       end
     else
-      @students = Student.all.order(:name)
+      @students = Student.all.order(:name).limit 200
     end
   end
 end
