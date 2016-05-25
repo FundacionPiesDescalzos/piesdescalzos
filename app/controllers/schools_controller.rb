3f73c6eba4d55fa89ac4ca96ba2820c780b9a578
fabianrios <faben02@gmail.com>
2016-05-25 09:52:38 +0200

    destroy schools

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -88,7 +88,7 @@ class SchoolsController < ApplicationController
   def destroy
     @school.destroy
     respond_to do |format|
-      format.html { redirect_to schools_url, notice: 'School was successfully destroyed.' }
+      format.html { redirect_to establishments_url, notice: 'School was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
